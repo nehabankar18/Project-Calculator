5e9c47baf4b0ef6e9c679befb26f14b3b44331a1
01fe22bca040 <01fe22bca040@kletech.ac.in>
2024-02-08 15:04:36 +0530

    program using math functions

---
+++ MathOperations.java
@@ -0,0 +1,25 @@
+public class MathOperations {
+    public double squareRoot(double a) {
+        return Math.sqrt(a);
+    }
+
+    public double power(double a, double b) {
+        return Math.pow(a, b);
+    }
+
+    public double absolute(double a) {
+        return Math.abs(a);
+    }
+
+    public double logarithm(double a) {
+        return Math.log(a);
+    }
+
+    public double sine(double a) {
+        return Math.sin(a);
+    }
+
+    public double cosine(double a) {
+        return Math.cos(a);
+    }
+}
