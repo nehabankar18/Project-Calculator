efa6e34bc3c011082ece1f425269febb3d3024e0
01fe22bca040 <01fe22bca040@kletech.ac.in>
2024-02-08 14:32:04 +0530

    simple program on geometrical area

---
+++ GeoArea.java
@@ -0,0 +1,10 @@
+public class GeoArea {
+ 
+    double area_of_triangle(float operand1, float operand2) {
+        return (0.5 * operand1 * operand2);
+    }
+    double area_of_rectangle(float operand1,float operand2)
+    {
+    return operand1*operand2;
+}
+}
