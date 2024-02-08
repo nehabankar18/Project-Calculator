6ca4747b1b89306aa37db39a47d7389215b213cc
01fe22bca040 <01fe22bca040@kletech.ac.in>
2024-02-08 14:43:42 +0530

    Simple program on arithmatic operations

---
+++ arithmatic.java
@@ -0,0 +1,14 @@
+public class arithmatic {
+    int cube(int a){
+       return a*a*a;
+    }
+    int square(int a){
+         return a*a;
+    }
+    int modulus(int a, int b){
+        return a%b;
+    }
+    int power(int a,int b){
+        return (int) Math.pow(a,b);
+    }
+}
